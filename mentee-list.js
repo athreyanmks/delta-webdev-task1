0f42e1d5893b8a5240d6f419e4713976894653e5
athreyanmks <30892530+athreyanmks@users.noreply.github.com>
2018-05-18 22:10:47 +0530

    Add files via upload

--- mentee-list.js
+++ mentee-list.js
@@ -5,29 +5,47 @@ var menteeArray= [];
 
 function dispMentee()
 {
+	var divi = document.createElement('div');
+	divi.setAttribute('id',j+'div');
+	divi.setAttribute('class','divisions')
+	divi.setAttribute('style','font-family : Verdana; font-size : 20px;')
+	
+	document.body.appendChild(divi);
+
+	setColor(j);
+
 	var para1 = document.createElement("P");      
 	//para1.setAttribute('id','para'+k[j])                 
 	
-	var t = document.createTextNode(menteeArray[j].name+' '+menteeArray[j].rat+' '+menteeArray[j].com);
+	var t = document.createTextNode('Name : ' + menteeArray[j].name + '  ||  ' + 'Rating : ' + menteeArray[j].rat);
 	para1.setAttribute('id',j+"para");   
+	para1.setAttribute('style','font-family : Verdana; font-size : 20px;')
 	para1.appendChild(t);                                          
-	document.body.appendChild(para1);
+	document.getElementById(j+'div').appendChild(para1);
+
+	var para2 = document.createElement('p');
+	para2.setAttribute('style','font-family : Verdana; font-size : 20px;')
+	var t2 = document.createTextNode('Comments : ' + menteeArray[j].com);
+	para2.appendChild(t2);
+	document.getElementById(j+'div').appendChild(para2);
 
 
 	var btn = document.createElement("BUTTON");
-	btn.setAttribute("key",j+"para");
+	btn.setAttribute("key",j+"div");
 	btn.setAttribute("id",j+"btn");     
-	btn.setAttribute("onclick"," this.parentElement.style.display = 'none'; ");   
+	btn.setAttribute("onclick"," this.parentElement.style.display = 'none'; "); 
+	btn.setAttribute("style","color : red; font-family : Verdana; font-size : 20px ; border-radius: 5px;");  
 	var b = document.createTextNode("Del");       
 	btn.appendChild(b);                                
-	document.getElementById(j+'para').appendChild(btn);
+	document.getElementById(j+'div').appendChild(btn);
 
 	var btn2 = document.createElement("BUTTON");
 	btn2.setAttribute("num",j);
 	btn2.setAttribute("onclick",'console.log(this.parentElement.id);console.log(parseInt(this.parentElement.id,10));editMentee(parseInt(this.parentElement.id,10));')
+	btn2.setAttribute('style','color : slateblue; font-family : Verdana; font-size : 20px; border-radius : 5px;')
 	var e = document.createTextNode("Edit");
 	btn2.appendChild(e);
-	document.getElementById(j+"para").appendChild(btn2);
+	document.getElementById(j+"div").appendChild(btn2);
 
 
 ++j;
@@ -82,22 +100,75 @@ function editMenteeInput()
 	rat = getInput('editRat');
 	com = getInput('editCom');
 
-	var editPara = document.getElementById(menteeIdNum+'para');
-	editPara.innerHTML= name + ' ' + rat + ' ' + com;
+	menteeArray[menteeIdNum].name = name;
+	menteeArray[menteeIdNum].rat = rat;
+	menteeArray[menteeIdNum].com = com;
+
+	var editDiv = document.getElementById(menteeIdNum+'div');
+	editDiv.innerHTML= '';
 	//document.getElementById('test').innerHTML=rat;
+	setColor(menteeIdNum);
+
+	var para1 = document.createElement("P");      
+	//para1.setAttribute('id','para'+k[j])                 
+	
+	var t = document.createTextNode('Name : ' + name + '  ||  ' + 'Rating : ' + rat);
+	para1.setAttribute('id',menteeIdNum+"para");  
+	para1.setAttribute('style','font-family : Verdana; font-size : 20px;') ;
+
+	para1.appendChild(t);                                          
+	document.getElementById(menteeIdNum+'div').appendChild(para1);
+
+	var para2 = document.createElement('p');
+	var t2 = document.createTextNode('Comments : ' + com);
+	para2.setAttribute('style','font-family : Verdana; font-size : 20px;')
+	para2.appendChild(t2);
+	document.getElementById(menteeIdNum+'div').appendChild(para2);
 
 	var btn = document.createElement("BUTTON");
 	btn.setAttribute("key",menteeIdNum+"para");
 	btn.setAttribute("id",menteeIdNum+"btn");     
-	btn.setAttribute("onclick"," this.parentElement.style.display = 'none'; ");   
+	btn.setAttribute("onclick"," this.parentElement.style.display = 'none'; ");
+	btn.setAttribute("style","color : red; font-family : Verdana; font-size : 20px; border-radius : 5px;");   
 	var b = document.createTextNode("Del");       
 	btn.appendChild(b);                                
-	document.getElementById(menteeIdNum+'para').appendChild(btn);
+	document.getElementById(menteeIdNum+'div').appendChild(btn);
 
 	var btn2 = document.createElement("BUTTON");
 	btn2.setAttribute("num",menteeIdNum);
 	btn2.setAttribute("onclick",'console.log(this.parentElement.id);console.log(parseInt(this.parentElement.id,10));editMentee(parseInt(this.parentElement.id,10));')
+	btn2.setAttribute("style","color : slateblue; font-family : Verdana; font-size : 20px; border-radius : 5px;");
 	var e = document.createTextNode("Edit");
 	btn2.appendChild(e);
-	document.getElementById(menteeIdNum+"para").appendChild(btn2);
+	document.getElementById(menteeIdNum+"div").appendChild(btn2);
+}
+
+function setColor(pointer)
+{
+	var dd = document.getElementById(pointer + 'div');
+
+	if(rat==1)
+	{
+		dd.setAttribute('style','border : 5px solid rgba(255,99,71,1);border-radius: 5px;padding-top: 10px;padding-bottom: 10px;background-color : rgba(255,99,71,0.8) ; margin-right : 250px;') ;
+	}
+
+	if(rat==2)
+	{
+		dd.setAttribute('style','border : 5px solid rgba(255,165,0,1);border-radius: 5px;padding-top: 10px;padding-bottom: 10px;background-color : rgba(255,165,0,0.8) ; margin-right : 250px;') ;
+	}
+
+	if (rat == 3) 
+	{
+		dd.setAttribute('style','border : 5px solid rgba(255,255,0,1);border-radius: 5px;padding-top: 10px;padding-bottom: 10px;background-color : rgba(255,255,0,0.8) ; margin-right : 250px;') ;
+	}
+
+	if (rat == 4) 
+	{
+		dd.setAttribute('style','border : 5px solid rgba(152,251,152,1);border-radius: 5px;padding-top: 10px;padding-bottom: 10px;background-color : rgba(152,251,152,0.8) ; margin-right : 250px;') ;
+	}
+
+	if (rat == 5) 
+	{
+		dd.setAttribute('style','border : 5px solid rgba(60,179,113,1);border-radius: 5px;padding-top: 10px;padding-bottom: 10px;background-color : rgba(60,179,113,0.8) ; margin-right : 250px;') ;
+	}
 }
